2a94324887e7c2e4667510d62a76bc6d6c20e730
Matti-Matias Sivula <matti-matias.sivula@khk.ee>
2020-11-13 14:43:37 +0200

    lause formuleerimine

---
+++ praks3/yl_2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+###skript mis loob lause###
+kes="Kaljulaid"
+mis_tegi=" lõpetas"
+kuna=" 1992.aastal"
+mille=" Tartu Ülikooli"
+mis_alal=" bioloogia erialal."
+lause=$kes$mis_tegi$kuna$mille$mis_alal
+echo
+echo $lause
+echo
