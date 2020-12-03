ecc23b1dbe1c6b70cc4fed9d424f73127da5fc0f
Matti-Matias Sivula <matti-matias.sivula@khk.ee>
2020-12-03 15:28:38 +0200

    püramiid

---
+++ praks7/yl_3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#
+echo
+echo -n "Sisesta ridade arv: "
+read rida
+for (( i=1; i<=$rida; i++ ))
+do
+echo -n  "$i."
+	for  (( j=1; j<=i; j++ )); do
+	echo -n  "*"
+	done
+echo ""
+done 
